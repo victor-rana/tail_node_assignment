35a214a30cd1d58743219ee76919a1f8dfabb9f2
ayushrana <ayush@getzyme.com>
2021-10-28 02:50:21 +0530

    files changes

--- app/src/main/java/com/example/tail/node/MainActivity.kt
+++ app/src/main/java/com/example/tail/node/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.example.tail.node
 
 import android.Manifest.permission
+import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+import android.R.attr
 import android.content.*
 import android.content.pm.PackageManager
 import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.os.IBinder
 import android.util.Log
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -20,19 +20,28 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import android.location.Criteria
 
 import android.location.LocationManager
-import android.os.Handler
-import android.os.Looper
-import java.io.FileOutputStream
-import java.io.IOException
-import java.io.OutputStreamWriter
-import android.os.Build
 
-import android.os.PowerManager
 import androidx.core.app.ActivityCompat.startActivityForResult
 
 import android.content.Intent
 import android.net.Uri
 import android.provider.Settings
+import android.R.attr.path
+import android.os.*
+import android.widget.Toast
+import java.io.*
+import android.R.attr.data
+import android.R.attr.path
+import android.R.attr.data
+
+
+
+
+
+
+
+
+
 
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
@@ -44,6 +53,8 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     var lat: Double? = null
     var lon: Double? = null
     private val IGNORE_BATTERY_OPTIMIZATION_REQUEST = 1002
+    var folder: File? = null
+    private val REQUEST_CODE_ASK_PERMISSIONS = 123
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -70,6 +81,16 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
             IntentFilter("onLocationBroadcast")
         )
 
+        if (ContextCompat.checkSelfPermission(this, WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            createFile()
+        } else {
+            // Request permission from the user
+            requestPermission()
+        }
+
+    }
+
+    fun createFile(){
         val handler = Handler()
         handler.postDelayed(object : Runnable {
             override fun run() {
@@ -78,7 +99,21 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
                 handler.postDelayed(this, 300000)//5 min delay
             }
         }, 0)
+    }
 
+    private fun requestPermission() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                WRITE_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat
+                .requestPermissions(
+                    this@MainActivity,
+                    arrayOf(WRITE_EXTERNAL_STORAGE),
+                    REQUEST_CODE_ASK_PERMISSIONS
+                )
+        }
     }
 
     private fun askIgnoreOptimization() {
@@ -105,7 +140,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
             Log.d("TAG", "requestPermissions: ")
             ActivityCompat.requestPermissions(
                 this,
-                arrayOf(permission.ACCESS_FINE_LOCATION),
+                arrayOf(permission.ACCESS_FINE_LOCATION,WRITE_EXTERNAL_STORAGE),
                 PERMISSION_REQUEST_CODE
             )
         }
@@ -124,6 +159,19 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         Log.d("TAG", "onRequestPermissionsResult: $requestCode")
+
+        if(requestCode == REQUEST_CODE_ASK_PERMISSIONS){
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission Granted
+                    createFile()
+                Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT)
+                    .show()
+            } else {
+                // Permission Denied
+                Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
 
         if (requestCode == PERMISSION_REQUEST_CODE) {
             if (grantResults.size > 0
@@ -170,6 +218,8 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     }
 
     fun onUpdateMap(lat: Double, lon: Double) {
+        this.lat = lat
+        this.lon = lon
         Log.d("TAG", "onUpdateMap: ")
         val latLng = LatLng(lat, lon)
         mMap!!.addMarker(MarkerOptions().position(latLng).title("Marker"))
@@ -194,13 +244,11 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
                     permission.ACCESS_COARSE_LOCATION
                 ) != PackageManager.PERMISSION_GRANTED
             ) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(permission.ACCESS_FINE_LOCATION),
+                    PERMISSION_REQUEST_CODE
+                )
                 return
             }
             locationManager.getLastKnownLocation(
@@ -222,15 +270,41 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         lat: String, lon: String,
     ) {
         val entryString =
-            "lat=$lat;lon=$lon;"
-        val fOut: FileOutputStream = openFileOutput(
-            "savedData.txt",
-            MODE_APPEND
-        )
-        val osw = OutputStreamWriter(fOut)
-        osw.write(entryString)
-        osw.flush()
-        osw.close()
+            "\nlat=$lat;lon=$lon;"
+
+
+        // Get the directory for the user's public pictures directory.
+        // Get the directory for the user's public pictures directory.
+        val path =
+            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS.toString() + "/TailNode/")
+
+        // Make sure the path directory exists.
+
+        // Make sure the path directory exists.
+        if (!path.exists()) {
+            // Make it, if it doesn't exit
+            path.mkdirs()
+        }
+
+        val file = File(path, "savedData.txt")
+
+        try {
+            file.createNewFile()
+            val fOut = FileOutputStream(file)
+            val myOutWriter = OutputStreamWriter(fOut)
+            myOutWriter.append(entryString)
+            myOutWriter.close()
+            fOut.close()
+        } catch (e: IOException) {
+            Log.e("Exception", "File write failed: $e")
+        }
+
+//        val fileout = openFileOutput("savedData.txt", MODE_APPEND)
+//        val outputWriter = OutputStreamWriter(fileout)
+//        outputWriter.write(entryString)
+//        outputWriter.close()
+        Log.d("TAG", "commitToFile: "+File(folder, "savedData.txt").path)
+        Toast.makeText(this, "Saved", Toast.LENGTH_SHORT).show()
     }
 
 
